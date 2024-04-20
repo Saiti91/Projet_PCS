8e5d089a1d99bc5295273cabe411d2fa8ce92e27
HomeDesktop <vellastephane91@gmail.com>
2024-04-20 17:56:19 +0200

    fix

--- src/common/middlewares/auth_middleware.js
+++ src/common/middlewares/auth_middleware.js
@@ -1,9 +1,11 @@
+// Importation des types Express
+// import { Request, Response } from "express";
+
 // Importation de express-jwt pour gérer la validation des JWT et des types de base d'Express
-import { expressjwt as jwt } from "express-jwt";
-import { Request, Response } from "express";
+const { expressjwt: jwt } =  require( "express-jwt");
 
 // Exportation par défaut du middleware jwt configuré
-export default jwt({
+module.exports = jwt({
     secret: "secret",             // La clé secrète utilisée pour signer les tokens JWT
     algorithms: ["HS256"],        // Liste des algorithmes de codage acceptés pour les tokens
     credentialsRequired: false,   // Spécifie si le middleware doit échouer si aucun token n'est fourni
