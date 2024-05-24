67bbf4b4b243330597ff3946e6c5801102e4c6b5
Stephane <vellastephane91@gmail.com>
2024-05-24 13:53:01 +0200

    Add commentary and calendar route + update init.sql

--- setupLinux.sh
+++ setupLinux.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 # Assurer que le script est exécuté en tant que superutilisateur (sudo)
 if [[ $EUID -ne 0 ]]; then
   echo "Ce script doit être exécuté en tant que root (sudo)" 1>&2
@@ -21,7 +22,7 @@ docker pull postgres:latest
 
 # Lancer le Docker de la base de données PostgreSQL avec le script SQL
 echo "Démarrage du conteneur Docker pour PostgreSQL..."
-docker run --name PCS_postgres -e POSTGRES_PASSWORD=password -d -v "$(pwd)/database:/docker-entrypoint-initdb.d" postgres:latest
+docker run --name PCS_postgres -e POSTGRES_PASSWORD=password -d -v "$(pwd)/database:/docker-entrypoint-initdb.d" -p 5432:5432 postgres:latest
 
 # Vérifier si Node.js est installé et l'installer si ce n'est pas le cas
 if ! command -v node &> /dev/null; then
